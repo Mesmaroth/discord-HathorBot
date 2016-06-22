e59bd737abab5982316b114ba00c488d48c832bb
Mesmaroth <mesmaroth666@live.com>
2016-06-21 18:28:57 -0700

    fix bugs, using asynchronuscall backs and error handling

--- apollobot.js
+++ apollobot.js
@@ -1,21 +1,17 @@
 var DiscordClient = require('discord.io');
 var uptimer = require('uptimer');
-var botLogin = require('./botLogin.json');
 var fs = require('fs');
 var ytdl = require('ytdl-core');
-var bot = new DiscordClient({token: botLogin.token, autorun: true})
+var botLogin = require('./botLogin.json');
+var bot = new DiscordClient({token: botLogin.token, autorun: true});
 var reboot = false;
 var queue = [];
 var playing = false;
 var streamer = {};
 var stayOnQueue = false;
 var stoppedAudio = false;
-var botVersion = "Apbot"
-try{botVersion = "Apbot "+ require("./package.json").version}
-catch(error){
-	if(error) return error
-};
-
+try{var botVersion = "Apbot "+ require("./package.json").version}
+catch(error){var botVersion; if(error) return console.error(error)};
 
 function botUptime(){
 	var upSeconds = Math.floor( uptimer.getAppUptime());
@@ -40,36 +36,60 @@ function setGame(game) {
 }
 
 function getTitleVideoID(url, callback){
-	ytdl.getInfo(url, function (error, data) {
-		if(error) return console.error(error);
-		video_id = data.video_id;
-		title = data.title;
-		for(var i = 0; i < queue.length; i++){
-			if(queue[i].video_id === video_id){
-				video_id = Math.floor(Math.random()*1000)+video_id;
+	try{
+		ytdl.getInfo(url, function (error, data) {
+			if(error) return callback(error);
+			video_id = data.video_id;
+			title = data.title;
+			for(var i = 0; i < queue.length; i++){
+				if(queue[i].video_id === video_id){
+					video_id = Math.floor(Math.random()*1000)+video_id;
+				}
 			}
-		}
-		callback(title, video_id);
-	});	
+			callback(null, title, video_id, Number(data.length_seconds));
+		});
+	}
+	catch(error) {return callback(error)};
 }
 
 function addSong(url, title, video_id, user, callback){
-	ytdl(url).pipe(fs.createWriteStream('./tempFiles/'+video_id+'.mp3'));
-	queue.push({
-		title: title,
-		video_id: video_id,
-		url: url,
-		user: user,
-		file: './tempFiles/'+video_id+'.mp3'
+	fs.access('./tempFiles', function(error){
+		if(error) {
+			fs.mkdir('./tempFiles', function(error){
+				if(error) return callback(error);
+				ytdl(url).pipe(fs.createWriteStream('./tempFiles/'+video_id+'.mp3'));
+				queue.push({
+					title: title,
+					video_id: video_id,
+					url: url,
+					user: user,
+					file: './tempFiles/'+video_id+'.mp3'
+				});
+			callback(null);
+			});
+		} else{
+			ytdl(url).pipe(fs.createWriteStream('./tempFiles/'+video_id+'.mp3'));
+			queue.push({
+				title: title,
+				video_id: video_id,
+				url: url,
+				user: user,
+				file: './tempFiles/'+video_id+'.mp3'
+			});
+			callback(null);
+		}
 	});
-
-	setTimeout(callback, 2500);	
+				
 }
 
 function isInVC(channelID){
 	var serverID = bot.serverFromChannel(channelID);
-	if(bot.servers[serverID].members[bot.id].hasOwnProperty('voice_channel_id')){
-		if(bot.servers[serverID].members[bot.id].voice_channel_id !== null) return true;
+	for(var channel in bot.servers[serverID].channels){
+		if(bot.servers[serverID].channels[channel].type === "voice"){
+			if(bot.id in bot.servers[serverID].channels[channel].members) return true;
+			break;
+		}
+
 	}
 
 	return false;
@@ -102,10 +122,12 @@ function removeSong(song){
 		}
 
 		if(fileExist){
-			fs.unlinkSync(filePath);
-			queue.splice(songIndex, 1);
-		}
-		else{
+			fs.unlink(filePath, function(error){
+				if(error) console.error(error);
+				queue.splice(songIndex, 1);
+			});
+			
+		} else{
 			queue.splice(songIndex, 1);
 		}
 
@@ -120,7 +142,7 @@ function playSong(){
 	setGame(song.title);
 
 	streamer.once('fileEnd', function() {
-		// Delete File and Remove song from queue.			
+		// Delete rile and remove song from queue.			
 		if(!stayOnQueue) removeSong(song);
 		stayOnQueue = false;
 		
@@ -135,16 +157,30 @@ function playSong(){
 	});
 }
 
+// Get the first voice ID in the server
 function getVoiceID(channelID){
 	var serverID = bot.serverFromChannel(channelID);
-	var voiceID = "";
-	for(var i in bot.servers[serverID].channels){					// Get the first voice ID in the server
+	for(var i in bot.servers[serverID].channels){					
 		if(bot.servers[serverID].channels[i].type === "voice"){
-			voiceID = bot.servers[serverID].channels[i].id;
-			break;
+			return bot.servers[serverID].channels[i].id;			
 		}
-	}	
-	return voiceID;
+	}
+}
+
+function joinVC () {
+	for (var server in bot.servers){
+		for(var channel in bot.servers[server].channels){
+			if(bot.servers[server].channels[channel].type === "voice"){
+				var channelID = bot.servers[server].channels[channel].id;
+				bot.joinVoiceChannel(channelID, function(){
+					bot.getAudioContext({channel: channelID, stero: true}, function(stream){
+				 		streamer = stream;
+				 	});	
+				});
+				return;
+			}
+		}
+	}
 }
 
 
@@ -153,6 +189,7 @@ bot.on('disconnected', function() {
 		reboot = false;
 		console.log("Connecting...");
 		setTimeout(bot.connect, 3000);
+		return;
 	}
 	process.exit();
 });
@@ -161,14 +198,9 @@ bot.on('ready', function (rawEvent) {
 	console.log("\nDiscord.io - Version: " + bot.internals.version);
     console.log("Username: "+bot.username + " - (" + bot.id + ")");
     console.log('\n');
-    setGame(botVersion);
-
-    bot.joinVoiceChannel("102910652766519296", function(){
-		bot.getAudioContext({channel: "102910652766519296", stero: true}, function(stream){
-			streamer = stream;
-		});	
-	});
-
+    if(process.argv[2]) setGame(process.argv[2]+ " " + botVersion);
+    else setGame(botVersion);
+    joinVC();
 });
 
 bot.on('message', function (user, userID, channelID, message, rawEvent){
@@ -181,25 +213,28 @@ bot.on('message', function (user, userID, channelID, message, rawEvent){
 		return;
 	}
 
-	if(message === ".writeout"){
-		fs.writeFileSync(bot.username+".json", JSON.stringify(bot, null, '\t'), 'utf8');
+	if(message === ".writeout"){		
+		fs.writeFile(bot.username+".json", JSON.stringify(bot, null, '\t'), 'utf8', function(error){
+			if(error) return console.error(error);
+			console.log("Logged bot properties.");
+		});
 	}
 
 	if(message.toLowerCase() === ".about"){
 		bot.sendMessage({
 			to: channelID,
-			message: "\n**Username:** "+bot.username+"\n**Author:** Mesmaroth\n**Written in:** Node.js\n**Version:**" +botVersion+ "\n**Library:** Discord.io\n**Library Version:** "+bot.internals.version+
+			message: "\n**Username:** "+bot.username+"\n**Author:** Mesmaroth\n**Written in:** Node.js\n**Version:** " +botVersion+ "\n**Library:** Discord.io\n**Library Version:** "+bot.internals.version+
 			"\n**Avatar:** https://goo.gl/LN6BvU"+"\n\n**Why:** This bot was created to replace the current shit musicbot that I got sick and tired of, a shitty bot written by shitty people."+
 			" So I've decided to write this bot from the ground up."
 
 		});
 	}
 
-	if(message.toLowerCase() === ".music" || message === ".help"){
+	if(message.toLowerCase() === ".music" || message.toLowerCase() === ".help"){
 		bot.sendMessage({
 			to: channelID,
-			message: '\n**Music**\n•`.play [URL]`: Adds and plays the music from the queue\n•`.stop`: Stop song\n•`.skip`: Skip song (Still Needs Work)\n•`.replay`: Replay song\n•`.readd`: Re-Add the currently playing song to queue\n•`.about`: About this bot\n'+
-			'•`.queue`: View the list of songs in queue\n•`.reboot`: Reboot the bot if something is wrong\n•`.uptime`: Up time of bot'
+			message: '\n**Music**\n•`.play [URL]`: Adds and plays the music from the queue\n•`.play`: If a song is in queue after it has been stopped before\n•`.stop`: Stop song\n•`.skip`: Skip the currently playing song\n•`.replay`: Replay song\n•`.readd`: Re-Add the currently playing song to queue\n•`.about`: About this bot\n'+
+			'•`.queue`: View the list of songs in queue\n•`.reboot`: Reboot the bot if something is wrong\n•`.uptime`: How long this bot has been online for'
 		});
 	}
 
@@ -239,20 +274,20 @@ bot.on('message', function (user, userID, channelID, message, rawEvent){
 	}
 
 	if(message.toLowerCase() === ".join"){
-		if(!(isInVC(channelID))){
-			var voiceID = getVoiceID("102910652766519296");
-		    bot.joinVoiceChannel(voiceID, function(){
+		if(!isInVC(channelID)){
+			var voiceID = getVoiceID(channelID);
+			bot.joinVoiceChannel(voiceID, function(){
 				bot.getAudioContext({channel: voiceID, stero: true}, function(stream){
-					streamer = stream;
+				streamer = stream;
 				});	
 			});
-		}
-		else{
+
+		} else {
 			bot.sendMessage({
 				to: channelID,
-				message: "*Already joined voice channel.*"
+				message: "Already in voice channel."
 			});
-		}		
+		}
 	}
 
 		
@@ -265,8 +300,7 @@ bot.on('message', function (user, userID, channelID, message, rawEvent){
 				streamer.stopAudioFile();
 				stayOnQueue = true;
 				stoppedAudio = true;				
-			}
-			else{
+			} else{
 				if(queue.length < 1){
 					bot.sendMessage({
 						to: channelID,
@@ -293,7 +327,7 @@ bot.on('message', function (user, userID, channelID, message, rawEvent){
 	}
 
 	if(message.toLowerCase() === ".skip"){
-		if(isInVC){
+		if(isInVC(channelID)){
 			if(playing){
 				var song = queue[0];
 				streamer.stopAudioFile();
@@ -314,8 +348,7 @@ bot.on('message', function (user, userID, channelID, message, rawEvent){
 							message: 'Playing *"' + song.title + '"*'
 						});
 					},1000);
-				}
-				else{
+				} else{
 					bot.sendMessage({
 						to: channelID,
 						message: "Queue is now empty."
@@ -344,8 +377,7 @@ bot.on('message', function (user, userID, channelID, message, rawEvent){
 					to: channelID,
 					message: '*"'+queue[0].title+'"* has been re-added back to queue.'
 				});
-			}
-			else{
+			} else{
 				if(queue.length < 1){
 					bot.sendMessage({
 						to: channelID,
@@ -372,8 +404,7 @@ bot.on('message', function (user, userID, channelID, message, rawEvent){
 				to: channelID,
 				message: songList[0] + "\n**Queued Songs**\nNo songs are queued."
 			})
-		}
-		else if(songList.length >= 2) {
+		} else if(songList.length >= 2) {
 			var queuedSongs = [];
 			for(var i = 1; i<songList.length; i++){
 				queuedSongs.push(songList[i]);
@@ -382,8 +413,7 @@ bot.on('message', function (user, userID, channelID, message, rawEvent){
 				to: channelID,
 				message: songList[0]+"\n**Queued Songs**\n"+queuedSongs.join('\n')
 			});
-		}
-		else{
+		} else{
 			bot.sendMessage({
 				to: channelID,
 				message: "No songs are queued."
@@ -391,53 +421,80 @@ bot.on('message', function (user, userID, channelID, message, rawEvent){
 		}
 	}
 
-	if(message.toLowerCase().search(".play") === 0){
-		if(message.search(' ') !== -1){
-			var message = message.split(' ');
-			var url = message[1];
-			try {
-				getTitleVideoID(url, function (title, video_id){
+	if(message.toLowerCase().search(/[.]play/) === 0){
+		if(isInVC(channelID)){
+			if(message.search(' ') !== -1){
+				var message = message.split(' ');
+				var url = message[1];
+				var voiceID = getVoiceID(channelID);
+				
+				if( !(userID in bot.servers[bot.serverFromChannel(channelID)].channels[voiceID].members) ){
+					bot.deleteMessage({
+						channel: channelID,
+						messageID: rawEvent.d.id
+					});
+
 					bot.sendMessage({
 						to: channelID,
-						message: '*"'+title+'"*  has been added to queue.'
+						message: "<@"+userID+"> you aren't in the voice channel."
 					});
-					addSong(url, title, video_id, user, function(){
-						if(playing === false){
-							var voiceID = getVoiceID(channelID);
-							setTimeout(playSong, 250);
-						}						
-					});
+					return;
+				}
+				
+				getTitleVideoID(url, function (error, title, video_id, length_seconds){
+					if(error) {
+						bot.sendMessage({
+							to: channelID,
+							message: "**Error:**```js\n" + error + "\n```" 
+						});
+						return;
+					}
 
-				});							
+					var seconds = length_seconds;
+					var hours = Math.trunc((seconds/60)/60);
+
+					// Video can only be 3 hours long.
+					if(hours < 3){
+						bot.sendMessage({
+							to: channelID,
+							message: '"' + title + '" has been added to queue.'
+						});
+						addSong(url, title, video_id, user, function(error){
+							if(error) return console.error(error);
+							if(playing === false){
+								setTimeout(playSong, 1000);							
+							}
+						});
+						
+					} else {
+						bot.deleteMessage({
+							channel: channelID,
+							messageID: rawEvent.d.id
+						});
+
+						bot.sendMessage({
+							to: channelID,
+							message: '**Error:** "'+title+'" is too long.'
+						});
+					}						
+				});			
+				return;
 			}
-			catch(error){
-				bot.sendMessage({
-					to: channelID,
-					message: "```javascript\n" + error + "\n```"
-				});
-			};
-			return;			
-		}		
 
-		if(isInVC(channelID)){
 			if(playing === false){
 				if(queue.length > 0){
-
 					bot.sendMessage({
 						to: channelID,
 						message: "Now playing *"+queue[0].title+"*"
 					});
-					var voiceID = getVoiceID(channelID);
 					playSong();
-				}
-				else{
+				} else{
 					bot.sendMessage({
 						to: channelID,
 						message: "No songs curretly in queue."
 					});
 				}			
 			}
-		} 				
+		}		 				
 	}
-
 });
