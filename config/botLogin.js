494325a4b9c7f5a5ad2618cabe5fa3f1aa0f78a7
Robert <Mesmaroth666@live.com>
2017-03-20 09:29:22 -0700

    Change login file to js instead of json

---
+++ config/botLogin.js
@@ -0,0 +1,8 @@
+// The bot only requires a token if you have a bot account
+// You can get a bot token at https://discordapp.com/developers/applications/me
+// If you don't want to use a bot account, you can instead use email and password
+module.exports = {
+	token: "{TOKEN}",
+	email: "{EMAIL}",
+	password: "{PASSWORD}"
+}
