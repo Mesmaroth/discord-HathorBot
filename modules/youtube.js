edd0da596cf36d40af5226244e5f38ff19c54d57
Robert <Mesmaroth666@live.com>
2017-05-20 14:19:35 -0700

    Fixed YT module

--- modules/youtube.js
+++ modules/youtube.js
@@ -1,7 +1,8 @@
 const ytdl = require('ytdl-core');
 const fs = require('fs');
+const path = require('path');
 const search = require('youtube-search')
-const youtubeAPI = require('../config/botLogin').youtubeAPI;
+const youtubeAPI = require(path.join(path.resolve(__dirname, '..'), 'config/botLogin')).youtubeAPI;
 
 module.exports.search = function(message, callback) {
 	var opts = {
@@ -10,7 +11,7 @@ module.exports.search = function(message, callback) {
 	};
 	 
 	search(message, opts, function(error, results) {
-	 	if(err) return callback(error);
+	 	if(error) callback(error);
 	 		 	
 	 	if(results){
 			var firstResult = results[0];
@@ -37,17 +38,13 @@ module.exports.getStream = function(url,callback){
 	callback(ytdl(url, ytdl_options));
 }
 
-module.exports.getFile = function(error, url, path, callback){
-	try{
-		if(fs.existsSync(path)){
-			callback(null);
-		}else{
-			ytdl(url, ytdl_options).pipe(fs.createWriteStream(path))
-			 .on('finish', ()=>{
-				callback(null);
-			 });
-		}	
-	} catch(error){
-		if(error) return callback(error);
-	}
+module.exports.getFile = function(url, path, callback){
+	if(fs.existsSync(path)){
+		callback();
+	}else{
+		ytdl(url, ytdl_options).pipe(fs.createWriteStream(path))
+		 .on('finish', ()=>{
+			callback();
+		 });
+	}	
 }
