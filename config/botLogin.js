fc38c6f059e4f75022918bef8ee8772266264881
Robert <Mesmaroth666@live.com>
2018-01-06 10:28:36 -0800

    Updated botLogin config

--- config/botLogin.js
+++ config/botLogin.js
@@ -1,11 +1,15 @@
 // The bot only requires a token if you have a bot account
 // You can get a bot token at https://discordapp.com/developers/applications/me
-// If you don't want to use a bot account, you can instead use email and password
-module.exports = {
-	token: "TOKEN",
-	youtubeAPI: "YOUTUBE_API_KEY",
 
-	//optional
-	email: "EMAIL",
-	password: "PASSWORD"
+// Youtube API is needed to use the search features 
+// otherwise you will keep getting bad request
+
+// Owner id is required to use the owner commands
+// you can get this id if you have developer options enabled
+// in discord. Then just right click your username and copy id
+
+module.exports = {
+	token: "TOKEN_KEY",
+	youtubeAPI: "YOTUBE_API_KEY",
+	owner_id: "OWNER_ID"
 }
